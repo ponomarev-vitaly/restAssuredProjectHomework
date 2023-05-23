d1501a8e2aa1c96fadbde89ba403c6c1d705a951
Vitaly Ponomarev <vp211082@gmail.com>
2023-05-23 15:25:04 +0300

    I modified the code in the RestAssuredRunner java class. I added http request as an Object inside RestAssured Library.

--- src/main/java/RestAssuredRunner.java
+++ src/main/java/RestAssuredRunner.java
@@ -1,5 +1,10 @@
+import io.restassured.RestAssured;
+
 public class RestAssuredRunner {
     public static void main(String[] args) {
-
+        RestAssured.given()
+                .log().all()
+                .baseUri("https://api.google.com")
+                .get();
     }
 }
